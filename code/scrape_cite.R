859ffb8442ba4cb788ad25adf1d8a4a44e04d3d6
Martin Schoemann <66668016+mschoemann@users.noreply.github.com>
2026-09-01 23:43:42 +0200

    add minor edits

--- code/scrape_cite.R
+++ code/scrape_cite.R
@@ -1,44 +1,11 @@
-source("functions.R")
+source("code/functions.R")
 
-# Store IDs
-gscholar_id <- 'DY2D56IAAAAJ'
-gscholar_page <- paste0("https://scholar.google.com/citations?user=", gscholar_id)
-
-sheet <- get_cv_sheet('pubs')
-id <- sheet %>%
-    filter(!is.na(id_scholar)) %>%
-    pull(id_scholar)
-id <- id[1]
-url <- paste0(
-    "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=DY2D56IAAAAJ&citation_for_view=DY2D56IAAAAJ:",
-    id
-)
+gscholar_id = 'EdZjQtsAAAAJ'
+stats = get_stats(gscholar_id)
+pubs = get_publications(gscholar_id) |>
+    select(year, title, pubid, cites) |>
+    arrange(desc(year))
 
 
-
-html <- xml2::read_html(url)
-rvest::html_elements(html, ".gs_scl") %>%
-    html_elements('a') %>%
-    html_text2()
-
-
-library(httr)
-page <- GET(url)
-page_content <- str_to_lower(content(page, as = "text"))
-str_split(page_content, 'cited by')[[1]]
-
-
-library(groundhog)
-groundhog.library(
-    c("scholar", "dplyr"),
-    "2025-07-01"
-)
-
-author_id = "EdZjQtsAAAAJ&hl"
-pubs = get_publications(author_id)
-
-pubs |>
-    select(year, title, pubid) |>
-    arrange(desc(year)) |>
-    View()
-
+write_csv(stats, "data/scholar_stats.csv")
+write_csv(pubs,  "data/scholar_pubs.csv")
